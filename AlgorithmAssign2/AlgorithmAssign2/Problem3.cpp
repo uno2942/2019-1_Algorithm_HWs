94671066cadaa9d56b3d759804549fe74ddc30a8
uno2942 <psb2942@gmail.com>
2019-03-11 23:49:18 +0900

    .

--- AlgorithmAssign2/AlgorithmAssign2/Problem3.cpp
+++ AlgorithmAssign2/AlgorithmAssign2/Problem3.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<list>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
 class Line {
@@ -8,36 +9,34 @@ public:
 	int l;
 	int r;
 	int y;
-	
 	bool operator <(const Line &a) const {
 		return this->l < a.l;
 	}
 };
-Line linelist[100][10000];
+vector<Line> linevec[20][100];
 int num[1000000];
-int num2[100];
-bool compare(Line a, Line b) {
-	if (a.y <= b.y)
-		return false;
-	else
-		return true;
-}
+int num2[20];
+int indexes[20000];
+int leftmost;
+int rightmost;
+
 int main() {
 	int num1;
-	
 	cin >> num1;
+	int l,r,y;
 	for (int i = 0; i < num1; i++)
 	{
 		cin >> num2[i];
 		for (int j = 0; j < num2[i]; j++) {
-			cin >> linelist[i][j].l >> linelist[i][j].r >> linelist[i][j].y;
-		}
-
-		sort(linelist[i], linelist[i] + num2[i]);
+			cin >> l >> r >> y;
+			linevec[i][y].push_back(Line(l, r));
+;		}
+		for(int j=0; j<100; j++)
+			sort(linevec[i][j].begin(), linevec[i][j].end());
 	}
 	for (int i = 0; i < num1; i++)
 	{
-		for (int j = 0; j < num2[i]; j++) {
+		for (int j = 0; j < 100; j++) {
 			cout << linelist[i][j].l << ' ' << linelist[i][j].r << ' ' << linelist[i][j].y << endl;
 		}
 	}
